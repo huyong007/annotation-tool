a407a242ab22049970e401b871687f1a9d337e0b
huyong007 <1341696804@qq.com>
2023-02-19 23:46:11 +0800

    revise gitignore

--- wasm-canvas/target/debug/deps/log-b7889717cf2e7465.d
+++ wasm-canvas/target/debug/deps/log-b7889717cf2e7465.d
@@ -1,8 +1,8 @@
-/Users/huyong/allfile/learn/annotation-tool/app/wasm-canvas/target/debug/deps/log-b7889717cf2e7465.rmeta: /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/lib.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/macros.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/serde.rs
+/Users/huyong/allfile/learn/annotation-tool/wasm-canvas/target/debug/deps/log-b7889717cf2e7465.rmeta: /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/lib.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/macros.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/serde.rs
 
-/Users/huyong/allfile/learn/annotation-tool/app/wasm-canvas/target/debug/deps/liblog-b7889717cf2e7465.rlib: /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/lib.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/macros.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/serde.rs
+/Users/huyong/allfile/learn/annotation-tool/wasm-canvas/target/debug/deps/liblog-b7889717cf2e7465.rlib: /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/lib.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/macros.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/serde.rs
 
-/Users/huyong/allfile/learn/annotation-tool/app/wasm-canvas/target/debug/deps/log-b7889717cf2e7465.d: /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/lib.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/macros.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/serde.rs
+/Users/huyong/allfile/learn/annotation-tool/wasm-canvas/target/debug/deps/log-b7889717cf2e7465.d: /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/lib.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/macros.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/serde.rs
 
 /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/lib.rs:
 /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/macros.rs:
--- wasm-canvas/target/debug/deps/wasm_bindgen_macro_support-94e1e5dabb095996.d
+++ wasm-canvas/target/debug/deps/wasm_bindgen_macro_support-94e1e5dabb095996.d
@@ -1,8 +1,8 @@
-/Users/huyong/allfile/learn/annotation-tool/app/wasm-canvas/target/debug/deps/wasm_bindgen_macro_support-94e1e5dabb095996.rmeta: /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-macro-support-0.2.84/src/lib.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-macro-support-0.2.84/src/parser.rs
+/Users/huyong/allfile/learn/annotation-tool/wasm-canvas/target/debug/deps/wasm_bindgen_macro_support-94e1e5dabb095996.rmeta: /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-macro-support-0.2.84/src/lib.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-macro-support-0.2.84/src/parser.rs
 
-/Users/huyong/allfile/learn/annotation-tool/app/wasm-canvas/target/debug/deps/libwasm_bindgen_macro_support-94e1e5dabb095996.rlib: /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-macro-support-0.2.84/src/lib.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-macro-support-0.2.84/src/parser.rs
+/Users/huyong/allfile/learn/annotation-tool/wasm-canvas/target/debug/deps/libwasm_bindgen_macro_support-94e1e5dabb095996.rlib: /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-macro-support-0.2.84/src/lib.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-macro-support-0.2.84/src/parser.rs
 
-/Users/huyong/allfile/learn/annotation-tool/app/wasm-canvas/target/debug/deps/wasm_bindgen_macro_support-94e1e5dabb095996.d: /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-macro-support-0.2.84/src/lib.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-macro-support-0.2.84/src/parser.rs
+/Users/huyong/allfile/learn/annotation-tool/wasm-canvas/target/debug/deps/wasm_bindgen_macro_support-94e1e5dabb095996.d: /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-macro-support-0.2.84/src/lib.rs /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-macro-support-0.2.84/src/parser.rs
 
 /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-macro-support-0.2.84/src/lib.rs:
 /Users/huyong/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-macro-support-0.2.84/src/parser.rs:
